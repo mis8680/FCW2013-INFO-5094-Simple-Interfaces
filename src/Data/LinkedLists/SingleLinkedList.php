4ef083f70292b2232ca6f08ac7057ee0c447fb18
mis8680 <mis8680@mis8680-virtual-machine.(none)>
2013-02-19 15:41:59 -0500

    update

---
+++ src/Data/LinkedLists/SingleLinkedList.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file is part of the Data package.
+ *
+ * For full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Data\LinkedLists;
