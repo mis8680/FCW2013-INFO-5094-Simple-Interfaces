c3e1061d87483d077c9bc327b209b3bb0b615f2a
peyton <moris_finch@hotmail.com>
2013-03-06 01:25:49 -0500

    No changes

--- src/Data/LinkedNode.php
+++ src/Data/LinkedNode.php
@@ -1,66 +1,66 @@
 <?php
 /**
- * This file is part of the Data package.
- *
- * For full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* This file is part of the Data package.
+*
+* For full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 namespace Data;
 
 /**
- *
- */
+*
+*/
 //require_once __DIR__ . '/bootstrap.php';
 
 /**
- * The SingleLinkedNode interface is implemented by all nodes that are to be linked.
- *
- * @author Insu Mun <i_mun@fanshaweonline.ca>
- * @package \Data
- * @version 1.0.0
- */
+* The SingleLinkedNode interface is implemented by all nodes that are to be linked.
+*
+* @author Insu Mun <i_mun@fanshaweonline.ca>
+* @package \Data
+* @version 1.0.0
+*/
 class LinkedNode implements \Data\ILinkedNode
 {
     /**
-     * key variable
-     *
-     * @access private
-     * @var int
-     * 
-     */
+* key variable
+*
+* @access private
+* @var int
+*
+*/
     private $key;
     
     /**
-     * value variable
-     *
-     * @access private
-     * @var mixed
-     * 
-     */
+* value variable
+*
+* @access private
+* @var mixed
+*
+*/
     private $value;
     
     /**
-     * Node variable
-     *
-     * @access private
-     * @var node
-     */
+* Node variable
+*
+* @access private
+* @var node
+*/
     protected $data;
     
     /**
-     * Next variable
-     *
-     * @access private
-     * @var ILinkedNode
-     */
+* Next variable
+*
+* @access private
+* @var ILinkedNode
+*/
     private $next;
     
     /**
-     * construct
-     *
-     * @access public
-     * @param Node 
-     */
+* construct
+*
+* @access public
+* @param Node
+*/
     public function __construct(\Data\Node $data = null, \Data\ILinkedNode $next = null)
     {
 
@@ -75,11 +75,11 @@ class LinkedNode implements \Data\ILinkedNode
     }
     
     /**
-     * Returns the next ILinkedNode.
-     *
-     * @access public
-     * @return ILinkedNode|null Returns the next ILinkedNode instance if it exists, otherwise returns NULL.
-     */
+* Returns the next ILinkedNode.
+*
+* @access public
+* @return ILinkedNode|null Returns the next ILinkedNode instance if it exists, otherwise returns NULL.
+*/
     public function getNext()
     {
         if (isset($this->next)) {
@@ -92,14 +92,14 @@ class LinkedNode implements \Data\ILinkedNode
     }
     
     /**
-     * Sets the next ILinkedNode instance.
-     *
-     * The `next` ILinkedNode should be the ILinkedNode instance that comes after
-     * this instance within a List.
-     *
-     * @access public
-     * @param ILinkedNode The ILinkedNode instance that is next.
-     */
+* Sets the next ILinkedNode instance.
+*
+* The `next` ILinkedNode should be the ILinkedNode instance that comes after
+* this instance within a List.
+*
+* @access public
+* @param ILinkedNode The ILinkedNode instance that is next.
+*/
     public function setNext(\Data\ILinkedNode $next = NULL)
     {
 
@@ -108,22 +108,22 @@ class LinkedNode implements \Data\ILinkedNode
   
     }
     /**
-     * Returns the key value for this node.
-     *
-     * @access public
-     * @return int Returns the key value.
-     */
+* Returns the key value for this node.
+*
+* @access public
+* @return int Returns the key value.
+*/
     public function getKey()
     {
         return isset($this->key) ? $this->key : null;
     }
     
     /**
-     * Sets the key value for this node.
-     *
-     * @access public
-     * @param mixed The key value.
-     */
+* Sets the key value for this node.
+*
+* @access public
+* @param mixed The key value.
+*/
     public function setKey($key)
     {
       
@@ -135,22 +135,22 @@ class LinkedNode implements \Data\ILinkedNode
     }
     
     /**
-     * Returns the value of this node (the real value assigned).
-     *
-     * @access public
-     * @return mixed The value.
-     */
+* Returns the value of this node (the real value assigned).
+*
+* @access public
+* @return mixed The value.
+*/
     public function getValue()
     {
         return isset($this->value)? $this->value : null;
     }
     
     /**
-     * Sets the value for this node.
-     *
-     * @access public
-     * @param mixed The value.
-     */
+* Sets the value for this node.
+*
+* @access public
+* @param mixed The value.
+*/
     public function setValue($value)
     {
      
@@ -162,11 +162,11 @@ class LinkedNode implements \Data\ILinkedNode
     }
     
     /**
-     * tostring method
-     *
-     * @access public
-     * @return string $value
-     */
+* tostring method
+*
+* @access public
+* @return string $value
+*/
     public function __toString()
     {
         return $this->getValue();
--- src/Data/Node.php
+++ src/Data/Node.php
@@ -1,50 +1,50 @@
 <?php
 /**
- * This file is part of the Data package.
- *
- * For full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* This file is part of the Data package.
+*
+* For full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 namespace Data;
 
 //require_once __DIR__ . '/INode.php';
 
 /**
- * Node class implemented by INode
- *
- * @author Insu Mun<mis8680@gmail.com>
- * @package Data
- * @version 1.0.0
- */
+* Node class implemented by INode
+*
+* @author Insu Mun<mis8680@gmail.com>
+* @package Data
+* @version 1.0.0
+*/
 class Node implements \Data\INode
 {
     /**
-     * key variable
-     *
-     * @access private
-     * @var int
-     * 
-     */
+* key variable
+*
+* @access private
+* @var int
+*
+*/
     protected $key;
     
     /**
-     * value variable
-     *
-     * @access private
-     * @var mixed
-     * 
-     */
+* value variable
+*
+* @access private
+* @var mixed
+*
+*/
     protected $value;
 
     
     /**
-     * Constructor
-     *
-     * @access public
-     * @param int $key
-     * @param mixed $value
-     * 
-     */
+* Constructor
+*
+* @access public
+* @param int $key
+* @param mixed $value
+*
+*/
     public function __construct($value = null, $key = null)
     {
         $this->setKey($key);
@@ -52,22 +52,22 @@ class Node implements \Data\INode
     }
     
     /**
-     * Returns the key value for this node.
-     *
-     * @access public
-     * @return int Returns the key value.
-     */
+* Returns the key value for this node.
+*
+* @access public
+* @return int Returns the key value.
+*/
     public function getKey()
     {
         return isset($this->key) ? $this->key : null;
     }
     
     /**
-     * Sets the key value for this node.
-     *
-     * @access public
-     * @param mixed The key value.
-     */
+* Sets the key value for this node.
+*
+* @access public
+* @param mixed The key value.
+*/
     public function setKey($key)
     {
         if($key) {
@@ -80,22 +80,22 @@ class Node implements \Data\INode
     }
     
     /**
-     * Returns the value of this node (the real value assigned).
-     *
-     * @access public
-     * @return mixed The value.
-     */
+* Returns the value of this node (the real value assigned).
+*
+* @access public
+* @return mixed The value.
+*/
     public function getValue()
     {
         return isset($this->value)? $this->value : null;
     }
     
     /**
-     * Sets the value for this node.
-     *
-     * @access public
-     * @param mixed The value.
-     */
+* Sets the value for this node.
+*
+* @access public
+* @param mixed The value.
+*/
     public function setValue($value)
     {
         if($value) {
