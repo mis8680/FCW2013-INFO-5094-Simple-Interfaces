c53f67f50959169acc86c23969b002b515e0bc4d
mis8680 <mis8680@mis8680-virtual-machine.(none)>
2013-03-07 20:36:48 -0500

    edit

--- src/Data/LinkedLists/Iterator.php
+++ src/Data/LinkedLists/Iterator.php
@@ -1,8 +1,25 @@
 <?php
+/**
+ * This file is part of the Data package.
+ *
+ * For full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Data\LinkedLists;
 
+/**
+ * @ignore
+ */
 require_once __DIR__ .  '/../Iterator.php';
 require_once __DIR__ .  '/../IteratorMode.php';
 
+/**
+ * The Iterator class 
+ *
+ * @author Insu Mun <i_mun@fanshaweonline.ca>
+ * @package Data
+ * @version 1.0.0
+ */
 class Iterator implements \Iterator
 {
     private $list;
